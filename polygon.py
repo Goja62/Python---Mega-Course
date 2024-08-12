72e8bf6c72b4803f392538537aa844972c68a015
Goja <gortnar@beograd.com>
2024-08-12 10:17:55 +0200

    Create a static web interface, Day 20

--- polygon.py
+++ polygon.py
@@ -1,7 +1,37 @@
-import random
-
-min = int(input("Enter a lower number: "))
-max = int(input("Enter a haier number: "))
+import FreeSimpleGUI as sg
 
 
-print(random.randint(min, max))
+def convert(feet, inches):
+    meters = feet * 0.3048 + inches * 0.0254
+    return meters
+
+
+sg.theme("Black")
+
+feet_label = sg.Text("Enter feet: ")
+feet_input = sg.Input(key="feet")
+
+inches_label = sg.Text("Enter inches: ")
+inches_input = sg.Input(key="inches")
+
+button = sg.Button("Convert")
+output_label = sg.Text("", key="output")
+exit_button = sg.Button("Exit")
+
+window = sg.Window("Convertor",
+                   layout=[[feet_label, feet_input],
+                           [inches_label, inches_input],
+                           [button, exit_button, output_label]])
+
+while True:
+    event, values = window.read()
+    match event:
+        case "Exit":
+            sg.popup("Please prowide a number")
+
+        case sg.WIN_CLOSED:
+            break
+    feet = float(values["feet"])
+    inches = float(values["inches"])
+    result = convert(feet, inches)
+    window["output"].update(value=f"{result} m", text_color="white")
